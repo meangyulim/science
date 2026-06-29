a3ae66286a7dd322e88e2826209f989d705c52b4
meangyulim <dlaalsrb10@gmail.com>
2026-06-30 08:41:28 +0900

    Implement update notice display and closing logic with localStorage in app.js

--- test/app.js
+++ test/app.js
@@ -1,4 +1,4 @@
-import { badWords, matchingItems, allQuizPool } from './data.js?v=4';
+import { badWords, matchingItems, allQuizPool } from './data.js?v=5';
 
 // 🚨 자가 진단 에러 출력 함수 먼저 등록
         window.showAlert = function(title, msg, type = 'success') {
